ba81d51a68bd0655eebdc4dac668efd135107dc7
Eugène <eugene.kiriloff@backmarket.com>
2025-03-14 19:28:36 +0100

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Eugène Kiriloff on 14/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
